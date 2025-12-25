f857bd22a4a709cd413dc9e1b178354c72a55280
asxmr <rania.azaoum@student.ehb.be>
2025-12-25 16:13:27 +0100

    Tabel aangemaakt voor Favorieten

---
+++ database/migrations/2025_12_25_150922_create_favorite_photos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favorite_photos', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignId('photo_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->timestamps();
+
+            $table->unique(['user_id', 'photo_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favorite_photos');
+    }
+};
