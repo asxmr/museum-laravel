119b4c19fd845a0741d325edbf6063fb69af225b
asxmr <rania.azaoum@student.ehb.be>
2025-12-26 14:38:29 +0100

    Profile koppelen aan layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
                 @endauth
 
                 @auth
-                    <a href="#"
+                    <a href="{{ route('profile.edit') }}"
                        class="px-4 sm:px-5 py-2 rounded-full transition text-sm"
                        style="@if(request()->routeIs('profile.*'))background-color:#591427;color:white;@else color:#7B1B38; @endif">
                         Profile
