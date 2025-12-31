3a43dc5b13d4d03536ebb3b3838efd65ede2b91e
asxmr <rania.azaoum@student.ehb.be>
2025-12-31 11:50:59 +0100

    Update lay-out en velden van het profiel formulier

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,19 +13,35 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post"
+          action="{{ route('profile.update') }}"
+          class="mt-6 space-y-6"
+          enctype="multipart/form-data">
         @csrf
         @method('patch')
 
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
+            <x-text-input id="name"
+                          name="name"
+                          type="text"
+                          class="mt-1 block w-full"
+                          :value="old('name', $user->name)"
+                          required
+                          autofocus
+                          autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
+            <x-text-input id="email"
+                          name="email"
+                          type="email"
+                          class="mt-1 block w-full"
+                          :value="old('email', $user->email)"
+                          required
+                          autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
@@ -33,7 +49,8 @@
                     <p class="text-sm mt-2 text-gray-800">
                         {{ __('Your email address is unverified.') }}
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <button form="send-verification"
+                                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             {{ __('Click here to re-send the verification email.') }}
                         </button>
                     </p>
@@ -47,6 +64,54 @@
             @endif
         </div>
 
+        <div>
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username"
+                          name="username"
+                          type="text"
+                          class="mt-1 block w-full"
+                          :value="old('username', $user->username)"
+                          autocomplete="username" />
+            <x-input-error class="mt-2" :messages="$errors->get('username')" />
+        </div>
+
+        <div>
+            <x-input-label for="birthday" :value="__('Birthday')" />
+            <x-text-input id="birthday"
+                          name="birthday"
+                          type="date"
+                          class="mt-1 block w-full"
+                          :value="old('birthday', optional($user->birthday)->format('Y-m-d'))" />
+            <x-input-error class="mt-2" :messages="$errors->get('birthday')" />
+        </div>
+
+        <div>
+            <x-input-label for="about_me" :value="__('About me')" />
+            <textarea id="about_me"
+                      name="about_me"
+                      rows="4"
+                      class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">{{ old('about_me', $user->about_me) }}</textarea>
+            <x-input-error class="mt-2" :messages="$errors->get('about_me')" />
+        </div>
+
+        <div>
+            <x-input-label for="photo" :value="__('Profile photo')" />
+
+            @if ($user->profile_photo_path)
+                <div class="mt-2 mb-3">
+                    <img src="{{ $user->profile_photo_url }}"
+                         alt="Profile photo"
+                         class="h-16 w-16 rounded-full object-cover">
+                </div>
+            @endif
+
+            <input id="photo"
+                   name="photo"
+                   type="file"
+                   class="mt-1 block w-full text-sm text-gray-500" />
+            <x-input-error class="mt-2" :messages="$errors->get('photo')" />
+        </div>
+
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
@@ -57,7 +122,9 @@
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
                     class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
+                >
+                    {{ __('Saved.') }}
+                </p>
             @endif
         </div>
     </form>
